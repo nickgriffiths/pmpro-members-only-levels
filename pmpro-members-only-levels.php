0cfd9ca99bc4997b974c022e65f45a668ea59cc0
Nick Griffiths <nicholasgriffiths@gmail.com>
2014-09-07 20:46:41 +0100

    Membership levels with '1 year' in the name are excluded from list when no user is logged in

--- pmpro-members-only-levels.php
+++ pmpro-members-only-levels.php
@@ -8,12 +8,20 @@ Author: Nick Griffiths
 Author URI: https://github.com/nickgriffiths
 */
 
-function pmpromoml_pmpro_levels_array($pmpro_levels)
-{
-	var_dump($pmpro_levels);
-	/*if(strpos($level->description, '1 Year') !== false) {
-		
-	}*/
+function pmpromoml_pmpro_levels_array($pmpro_levels) {
+	
+	global $current_user;
+	
+	$is_member = isset($current_user->membership_level->ID);
+	foreach($pmpro_levels as $key => $level) {
+		if(strpos($level->name, '1 year') !== false) {
+			if (!$is_member) {
+				unset($pmpro_levels[$key]);
+			}
+		}
+	}
+	
+	
 	return $pmpro_levels;
 }
 add_filter("pmpro_levels_array", "pmpromoml_pmpro_levels_array");
